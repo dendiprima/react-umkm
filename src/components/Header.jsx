d024065e21394608681f999b328e09d79732ec32
zuchals <zuchals@kali.net>
2023-08-10 23:28:26 +0700

    merubah header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,15 @@
+import {RxHamburgerMenu} from 'react-icons/rx'
+
 export default function Header() {
   return (
-    <div>
-      <div className="text-7xl font-semibold text-center my-8">Hallo World !!</div>
-    </div>
+    <>
+    <nav className="py-9 px-4 shadow-md">
+      <div className="flex justify-between">
+        <img src="/vite.svg" alt="" />
+        <button className="py-2 px-3 bg-slate-600 text-white rounded-md hover:bg-white hover:text-slate-600 hover:ring-2"><RxHamburgerMenu/></button>
+
+      </div>
+    </nav>
+    </>
   )
 }
