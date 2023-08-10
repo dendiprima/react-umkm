0ab1b6cd0497f761e5244852dd0558f949156d34
zuchals <zuchals@kali.net>
2023-08-10 22:49:22 +0700

    menambahkan components header

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import Header from "./components/Header"
+
 export default function App() {
   return (
     <>
-    <div className="text-7xl font-semibold text-center my-8">Hallo</div>
+      <Header/>
     </>
   )
 }
