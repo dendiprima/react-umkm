29d14b4d56a672d8b77dfbc5edc0fa848f9a465b
zuchals <zuchals@gmail.com>
2023-08-12 04:02:03 +0700

    navbar commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,41 +1,63 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
 import {AiOutlineHome, AiOutlineMenu} from 'react-icons/ai'
 import LogoJambe from "../assets/img/logo-jambe.png"
 import React, {useState} from 'react'
 
 export default function Navbar() {
+
   const [nav, setNav] = useState(false);
   const handleNav = () => {
     setNav(!nav);
   }
   return (
-    <>
-    <nav className="py-5 px-4 shadow-md">
-      <div className="flex justify-between items-center max-h-full order-1">
-        <a href="#root">
-          <img src={LogoJambe} alt="logo-jambe" className='w-12'/>
-        </a>
-        <div className='py-3 px-3 bg-slate-500 rounded-lg hover:bg-white hover:ring-2 cursor-pointer group absolute order-2'>
-          < AiOutlineMenu size={20} onClick={handleNav} className="text-white text-base group-hover:text-slate-600" />
-          {
-            nav ? (
-              <div className='fixed w-full h-screen bg-white/90 flex flex-col justify-center items-center z-100'>
-                <a href="#main" className='w-[75%] flex justify-center items-center rounded-full'>
-                  <AiOutlineHome size={20}/>
-                  <span className='pr-36'>Home</span>
-                </a>
-              </div>
-            ): (
-              <div>
-
-              </div>
-            )
-          }
+    <div>
+      <nav className=' justify-between items-center flex md:py-4 px-4 md:justify-around shadow-md bg-opacity-50'>
+        <div className='flex items-center'>
+          <a href="#home">
+            <img src={LogoJambe} alt="Logo Jambe" className='w-28' />
+          </a>
+        <h1 className='font-bold text-4xl m-6 md:visible invisible'>
+        UMKM Jambe
+        </h1>
         </div>
-          {/* <div>
-            <button className='grow bg-slate-500 px-6 py-4 font-bold text-white rounded-full text-sm hover:bg-white hover:text-slate-500 hover:ring-2'>Sign Up</button>
-          </div> */}
-      </div>
-    </nav>
-    </>
+
+        <button className=' bg-slate-500 rounded-md py-3 px-4 hover:bg-white hover:ring-2 group md:hidden'>
+          <AiOutlineMenu className='text-white group-hover:text-slate-500 z-[99]' onClick={handleNav}/>
+        {
+          nav ? (
+            <div className='flex flex-col gap-y-3 w-full h-screen items-center justify-center mx-auto fixed right-20 top-8 bg-white/90'>
+              <a href="#home" className='flex items-center justify-center w-full rounded-lg shadow-lg bg-slate-300/90 gap-4'>
+                <AiOutlineHome size={20}/>
+                <span className='py-6'>Home</span>
+              </a>
+              <a href="#home" className='flex items-center justify-center w-full rounded-lg shadow-lg bg-slate-300/90 gap-4'>
+                <AiOutlineHome size={20}/>
+                <span className='py-6'>Home</span>
+              </a>
+              <a href="#home" className='flex items-center justify-center w-full rounded-lg shadow-lg bg-slate-300/90 gap-4'>
+                <AiOutlineHome size={20}/>
+                <span className='py-6'>Home</span>
+              </a>
+              <a href="#home" className='flex items-center justify-center w-full rounded-lg shadow-lg bg-slate-300/90 gap-4'>
+                <AiOutlineHome size={20}/>
+                <span className='py-6'>Home</span>
+              </a>
+              <a href="#home" className='flex items-center justify-center w-full rounded-lg shadow-lg bg-slate-300/90 gap-4'>
+                <AiOutlineHome size={20}/>
+                <span className='py-6'>Home</span>
+              </a>
+            </div>
+            
+          )
+          : (
+            <div>
+
+            </div>
+          )   
+        }
+        </button>
+      </nav>
+    </div>
   )
 }
