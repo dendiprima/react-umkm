941b38b311b4aad28a3c6989e93a86d0e8fd6e18
zuchals <zuchals@gmail.com>
2023-08-13 18:08:05 +0700

    navbar jadi

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,17 +16,23 @@ export default function Navbar() {
   return (
     <div>
       <nav className='py-5 bg-white top-0 border-b border-slate-100'>
-        <div className='container md:px-12 md:flex md:items-center md:justify-center'>
-          <div className='flex items-center justify-between'>
-            <a href="#root" className='flex items-center'>
-              <img src={LogoJambe} alt="Logo Jambe" className='m-2 w-[12%]'/>
+        <div className='container md:flex md:items-center md:justify-center'>
+          <div className='flex items-center md:flex-1 justify-between'>
+            <a href="#root" className='flex items-center mx-10 gap-x-4 md:mx-24 md:gap-x-6'>
+              <img src={LogoJambe} alt="Logo Jambe" className=' w-[12%]'/>
               <span className='text-xl text-slate-800 font-sans font-bold'>UMKM Jambe</span>
             </a>
-            <span className='mx-2 text-xl bg-slate-100 p-2 rounded-lg md:hidden cursor-pointer ' onClick={handleNav}>
+            <div className='flex flex-grow gap-x-6 right-24 absolute invisible md:visible'>
+              <a href="" className='font-sans font-semibold text-lg'>Home</a>
+              <a href="" className='font-sans text-lg hover:border-b-2 hover:border-slate-700'>Nama UMKM</a>
+              <a href="" className='font-sans text-lg hover:border-b-2 hover:border-slate-700'>Kontak Kami</a>
+              <a href="" className='font-sans text-lg hover:border-b-2 hover:border-slate-700'>Tentang Kami</a>
+            </div>
+            <span className='mx-4 text-xl bg-slate-100 p-2 rounded-lg md:hidden cursor-pointer' onClick={handleNav}>
               <AiOutlineMenu className=''/>
               {
                 nav? (
-                  <div className='fixed w-full h-screen bg-white/50 flex flex-col md:flex-grow justify-center items-center z-20 left-0 top-0 gap-y-2'>
+                  <div className='fixed w-full h-screen bg-white/50 flex flex-col justify-center items-center z-20 left-0 top-0 gap-y-2'>
                     <a href="#home" className='flex items-center justify-center w-[75%] rounded-full shadow-md bg-gray-100 p-2 cursor-pointer hover:scale-110 ease-in duration-200'>
                       <AiOutlineHome size={20}/>
                       <span className='pl-4 text-base'>Home</span>
