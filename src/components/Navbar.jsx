6f5edc4507f4b4c772d687aa3f241476af86f14b
zuchals <zuchals@gmail.com>
2023-08-13 15:41:09 +0700

    merubah navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,63 +1,56 @@
 /* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
 import {AiOutlineHome, AiOutlineMenu} from 'react-icons/ai'
+import {BsCart4} from 'react-icons/bs'
+import {RiContactsLine} from 'react-icons/ri'
+import {FcAbout} from 'react-icons/fc'
+
 import LogoJambe from "../assets/img/logo-jambe.png"
 import React, {useState} from 'react'
 
 export default function Navbar() {
-
   const [nav, setNav] = useState(false);
   const handleNav = () => {
     setNav(!nav);
   }
   return (
     <div>
-      <nav className=' justify-between items-center flex md:py-4 px-4 md:justify-around shadow-md bg-opacity-50'>
-        <div className='flex items-center'>
-          <a href="#home">
-            <img src={LogoJambe} alt="Logo Jambe" className='w-28' />
-          </a>
-        <h1 className='font-bold text-4xl m-6 md:visible invisible'>
-        UMKM Jambe
-        </h1>
+      <nav className='py-5 bg-white top-0 border-b border-slate-100'>
+        <div className='container md:px-12 md:flex md:items-center md:justify-center'>
+          <div className='flex items-center justify-between'>
+            <a href="#root" className='flex items-center'>
+              <img src={LogoJambe} alt="Logo Jambe" className='m-2 w-[12%]'/>
+              <span className='text-xl text-slate-800 font-sans font-bold'>UMKM Jambe</span>
+            </a>
+            <span className='mx-2 text-xl bg-slate-100 p-2 rounded-lg md:hidden cursor-pointer ' onClick={handleNav}>
+              <AiOutlineMenu className=''/>
+              {
+                nav? (
+                  <div className='fixed w-full h-screen bg-white/50 flex flex-col md:flex-grow justify-center items-center z-20 left-0 top-0 gap-y-2'>
+                    <a href="#home" className='flex items-center justify-center w-[75%] rounded-full shadow-md bg-gray-100 p-2 cursor-pointer hover:scale-110 ease-in duration-200'>
+                      <AiOutlineHome size={20}/>
+                      <span className='pl-4 text-base'>Home</span>
+                    </a>
+                    <a href="#home" className='flex items-center justify-center w-[75%] rounded-full shadow-md bg-gray-100 p-2 cursor-pointer hover:scale-110 ease-in duration-200'>
+                      <BsCart4 size={20}/>
+                      <span className='pl-4 text-base'>Nama UMKM</span>
+                    </a><a href="#home" className='flex items-center justify-center w-[75%] rounded-full shadow-md bg-gray-100 p-2 cursor-pointer hover:scale-110 ease-in duration-200'>
+                      <RiContactsLine size={20}/>
+                      <span className='pl-4 text-base'>Kontak Kami</span>
+                    </a><a href="#home" className='flex items-center justify-center w-[75%] rounded-full shadow-md bg-gray-100 p-2 cursor-pointer hover:scale-110 ease-in duration-200'>
+                      <FcAbout size={20}/>
+                      <span className='pl-4 text-base'>Tentang Kami</span>
+                    </a>
+                  </div>
+                )
+                : (
+                  ''
+                )}
+            </span>
+          </div>
         </div>
-
-        <button className=' bg-slate-500 rounded-md py-3 px-4 hover:bg-white hover:ring-2 group md:hidden'>
-          <AiOutlineMenu className='text-white group-hover:text-slate-500 z-[99]' onClick={handleNav}/>
-        {
-          nav ? (
-            <div className='flex flex-col gap-y-3 w-full h-screen items-center justify-center mx-auto fixed right-20 top-8 bg-white/90'>
-              <a href="#home" className='flex items-center justify-center w-full rounded-lg shadow-lg bg-slate-300/90 gap-4'>
-                <AiOutlineHome size={20}/>
-                <span className='py-6'>Home</span>
-              </a>
-              <a href="#home" className='flex items-center justify-center w-full rounded-lg shadow-lg bg-slate-300/90 gap-4'>
-                <AiOutlineHome size={20}/>
-                <span className='py-6'>Home</span>
-              </a>
-              <a href="#home" className='flex items-center justify-center w-full rounded-lg shadow-lg bg-slate-300/90 gap-4'>
-                <AiOutlineHome size={20}/>
-                <span className='py-6'>Home</span>
-              </a>
-              <a href="#home" className='flex items-center justify-center w-full rounded-lg shadow-lg bg-slate-300/90 gap-4'>
-                <AiOutlineHome size={20}/>
-                <span className='py-6'>Home</span>
-              </a>
-              <a href="#home" className='flex items-center justify-center w-full rounded-lg shadow-lg bg-slate-300/90 gap-4'>
-                <AiOutlineHome size={20}/>
-                <span className='py-6'>Home</span>
-              </a>
-            </div>
-            
-          )
-          : (
-            <div>
-
-            </div>
-          )   
-        }
-        </button>
       </nav>
+
     </div>
   )
 }
