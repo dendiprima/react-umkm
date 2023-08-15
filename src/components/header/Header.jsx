1be6fe0826c544adff2c3487caacddcd59b8f901
zuchals <zuchals@gmail.com>
2023-08-15 15:25:29 +0700

    multi page

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,22 +3,23 @@ import Navbar from '../navbar/Navbar';
 
 export default function Header() {
   return (
-    <header className="bg-[url('../../src/assets/img/umkm.jpg')] bg-no-repeat h-fit bg-cover bg-center bg-blend-screen">
-      <Navbar/>
-      <div className="relative isolate px-6 pt-14 lg:px-8">
-        <div
-          className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
-          aria-hidden="true">
+    <header className="bg-[url('../../src/assets/img/umkm.jpg')] bg-no-repeat w-full h-screen bg-cover bg-center bg-blend-screen">
+      <div className='w-full h-screen bg-white/30 absolute top-0 left-0'>
+        <Navbar/>
+        <div className="relative isolate px-6 pt-14 lg:px-8">
           <div
+            className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+            aria-hidden="true">
+            <div
             // className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
             // style={{
             //   clipPath:
             //     'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
             // }}
-          />
-        </div>
-        <div className="mx-auto max-w-3xl py-32 sm:py-48 lg:py-56">
-          {/* <div className="hidden sm:mb-8 sm:flex sm:justify-center">
+            />
+          </div>
+          <div className="mx-auto max-w-3xl py-32 sm:py-48 lg:py-56">
+            {/* <div className="hidden sm:mb-8 sm:flex sm:justify-center">
             <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
               Announcing our next round of funding.{' '}
               <a href="#" className="font-semibold text-indigo-600">
@@ -27,56 +28,58 @@ export default function Header() {
               </a>
             </div>
           </div> */}
-          <div className="text-center">
-            <div className="text-4xl font-bold tracking-tight text-gray-100 sm:text-6xl">
-              Selamat Datang di Website Resmi <br/>
-              <TypeAnimation
-                sequence={[
-                  // Same substring at the start will only be typed out once, initially
-                  'UMKM Pasar Daru',
-                  1000,
-                  'UMKM Go Online',
-                  1000, // wait 1s before replacing "Mice" with "Hamsters"
-                  'UMKM Go Digital',
-                  1000,
-                ]}
-                wrapper="span"
-                speed={35}
-                style={{ fontSize: '1em', display: 'inline-block' }}
-                repeat={Infinity}
-              />
-            </div>
-            <p className="mt-8 text-lg leading-8 text-gray-100">
-              <span className='shadow-gray-900 bg-[#0078d7]'>
-                Dalam rangka meningkatkan ekonomi yang stabil dan berkelanjutan, Mari tingkatkan pertumbuhan UMKM untuk Indonesia yang lebih maju.
-              </span> 
-            </p>
-            <div className="mt-10 flex items-center justify-center gap-x-6">
-              <a
-                href="#"
-                className="rounded-md bg-white text-[#0078d7] hover:text-white px-3.5 py-2.5 text-sm font-semibold shadow-sm hover:bg-[#0078d7]/90 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#0078d7] leading-3 tracking-widest"
-              >
-                Join Now
-              </a>
-              {/* <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+            <div className="text-center">
+              <div className="text-4xl font-bold tracking-tight text-gray-800 sm:text-6xl">
+                Selamat Datang di Website Resmi <br />
+                <TypeAnimation
+                  sequence={[
+                    // Same substring at the start will only be typed out once, initially
+                    'UMKM Pasar Daru',
+                    1000,
+                    'UMKM Go Online',
+                    1000, // wait 1s before replacing "Mice" with "Hamsters"
+                    'UMKM Go Digital',
+                    1000,
+                  ]}
+                  wrapper="span"
+                  speed={35}
+                  style={{ fontSize: '1em', display: 'inline-block' }}
+                  repeat={Infinity}
+                />
+              </div>
+              <p className="mt-8 text-xl md:text-2xl md:leading-10 leading-8 text-gray-800">
+                <span className=''>
+                  Dalam rangka meningkatkan ekonomi yang stabil dan berkelanjutan, Mari tingkatkan pertumbuhan UMKM untuk Indonesia yang lebih maju.
+                </span>
+              </p>
+              <div className="mt-10 flex items-center justify-center gap-x-6">
+                <a
+                  href="/login"
+                  className="rounded-md bg-white text-[#0078d7] hover:text-white px-3.5 py-2.5 text-sm font-semibold shadow-sm hover:bg-[#0078d7]/90 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#0078d7] leading-3 tracking-widest"
+                >
+                  Join Now
+                </a>
+                {/* <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
                 Learn more <span aria-hidden="true">→</span>
               </a> */}
+              </div>
             </div>
           </div>
-        </div>
-        <div
-          className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
-          aria-hidden="true"
-        >
           <div
+            className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
+            aria-hidden="true"
+          >
+            <div
             // className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
             // style={{
             //   clipPath:
             //     'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
             // }}
-          />
+            />
+          </div>
         </div>
       </div>
+      
     </header>
   )
 }
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -18,7 +18,7 @@ export default function Navbar() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
   return (
     <>
-    <nav className="absolute inset-x-0 top-0 z-50 bg-slate-500/20">
+    <nav className="absolute inset-x-0 top-0 z-50 bg-gray-100/10 shadow-md">
       <div className="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
       <div className="flex lg:flex-1">
         <a href="/" className="-m-1.5 p-1.5 flex ">
@@ -26,7 +26,7 @@ export default function Navbar() {
             className="h-16 w-auto rounded-full bg-slate-50"
             src={logoJambe}
             alt="" />
-          <span className="text-lg font-semibold my-auto ml-4 text-slate-100 shadow-gray-950">UMKM Pasar Daru</span>
+          <span className="text-lg font-semibold leading-6 my-auto ml-4 text-gray-800">UMKM Pasar Daru</span>
         </a>
       </div>
       <div className="flex lg:hidden">
@@ -36,13 +36,13 @@ export default function Navbar() {
           onClick={() => setMobileMenuOpen(true)}
         >
           <span className="sr-only">Open main menu</span>
-          <Bars3Icon className="h-6 w-6 text-gray-100" aria-hidden="true" />
+          <Bars3Icon className="h-6 w-6 text-gray-800" aria-hidden="true" />
         </button>
       </div>
       <div className="hidden lg:flex lg:gap-x-12 ">
         {navigation.map((item) => (
           <a key={item.name} href={item.href}>
-            <span className="text-sm font-semibold leading-6 shadow-gray-900 text-gray-100 hover:text-gray-300">
+            <span className="text-md font-semibold leading-6 text-gray-800 hover:text-gray-500">
               {item.name}
             </span>
           </a>
@@ -82,7 +82,7 @@ export default function Navbar() {
                   <a
                     key={item.name}
                     href={item.href}
-                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-slate-100"
                   >{item.name}
                   </a>
                 ))}
